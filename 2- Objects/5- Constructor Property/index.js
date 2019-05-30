6b8fc0c0981c310bdabe63d222b08410f4e9c3a6
Nathan Swim <nathan.swim@med2020.ca>
2019-05-30 08:59:45 -0400

    Chapter 6

---
+++ 2- Objects/5- Constructor Property/index.js
@@ -0,0 +1,34 @@
+﻿// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+
+// constructor function
+function Circle(radius) {
+    console.log(this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const another = new Circle(1); // must use new; 1) creates an empty object 2) sets this to equal empty object.
+
+console.log(another.constructor);
+console.log(circle.constructor);
+
+let x = {};
+// let x = new Object();
+
+new String(); // '', "", ``
+
+new Boolean(); // true, false
+
+new Number(); // 1, 2, 3, ...
