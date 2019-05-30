d77261ebe91d2ccc58141aaafaafaf8305c36101
Nathan Swim <nathan.swim@med2020.ca>
2019-05-30 14:04:43 -0400

    chapter 12

--- 2- Objects/12- Getters and Setters/index.js
+++ 2- Objects/12- Getters and Setters/index.js
@@ -5,18 +5,33 @@
     // we want to hide this property ...
     let defaultLocation = { x: 0, y: 0 };
 
-    // ... and hide this function
-    let computeOptimumLocation = function() {
-        // do some magic here.
-    };
-
     this.draw = function() {
-        let x, y;
-        computeOptimumLocation();
-
         console.log('draw');
     };
+
+    // solution 1
+    this.getDefaultLocation = function() {
+        return defaultLocation;
+    };
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if (!value.x || !value.y) throw new Error('Invalid location.');
+            defaultLocation = value;
+        }
+    });
 }
 
 const circle = new Circle(10);
 circle.draw();
+
+console.log(circle.getDefaultLocation());
+console.log(circle.defaultLocation);
+
+// circle.defaultLocation = 1; // returns error
+circle.defaultLocation = { x: 1, y: 2 };
+
+console.log(circle.defaultLocation);
