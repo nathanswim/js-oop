004b14fdaa87ccb0fa1dc78d522fdbe5c1f5531c
Nathan Swim <nathan.swim@med2020.ca>
2019-05-30 08:29:16 -0400

    chapters 2-4

---
+++ 2- Objects/3- Factories/index.js
@@ -0,0 +1,12 @@
+﻿// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+circle.draw();
---
+++ 2- Objects/4- Constructors/index.js
@@ -0,0 +1,25 @@
+﻿// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+
+// constructor function
+function Circle(radius) {
+    console.log(this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const another = new Circle(1);
+another.draw();
+
+const other = Circle(1);
