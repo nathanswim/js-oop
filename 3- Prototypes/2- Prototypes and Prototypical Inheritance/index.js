30b0f3ced5662d0639147bdcb9a9f864048f3bae
Nathan Swim <nathan.swim@med2020.ca>
2019-05-30 15:55:27 -0400

    Unit 3 chapter 4

---
+++ 3- Prototypes/2- Prototypes and Prototypical Inheritance/index.js
@@ -0,0 +1,12 @@
+﻿let x = {};
+console.log('x', x);
+
+let y = {};
+console.log('y', y);
+
+console.log(Object.getPrototypeOf(x));
+console.log(Object.getPrototypeOf(y));
+
+console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y));
+
+// NOTE: the x.__proto__ property is deprecated.
