0bce30f0c12378a4316d4067619e07b462f169bb
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 16:00:41 -0400

    Unit 4 Chapter 6

---
+++ 4- Prototypical Inheritance/6- Polymorphism/index.js
@@ -0,0 +1,30 @@
+﻿function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
+function Shape() {}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+function Circle() {}
+
+extend(Circle, Shape);
+
+Circle.prototype.duplicate = function() {
+    console.log('duplicate Circle');
+};
+
+function Square() {}
+
+extend(Square, Shape);
+
+Square.prototype.duplicate = function() {
+    console.log('duplicate Square');
+};
+
+const shapes = [new Circle(), new Square()];
+
+for (let shape of shapes) shape.duplicate();
