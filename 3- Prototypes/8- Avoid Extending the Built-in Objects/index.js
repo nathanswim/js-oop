3698997d2d2322b7dedbbfc931a3c94106fd8dcd
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 13:45:53 -0400

    Unit 3 chapter 1

--- 3- Prototypes/8- Avoid Extending the Built-in Objects/index.js
+++ 3- Prototypes/8- Avoid Extending the Built-in Objects/index.js
@@ -1,7 +1,24 @@
-﻿// You should not modify the built in prototypes.
-Array.prototype.shuffle = function() {
-    console.log('shuffling the array');
+﻿function Circle(radius) {
+    // instance members
+    this.radius = radius;
+
+    this.move = function() {
+        console.log('move');
+    };
+}
+
+// prototype members
+Circle.prototype.draw = function() {
+    console.log('draw');
 };
 
-const array = [];
-array.shuffle();
+const c1 = new Circle(1);
+
+// only returns instance memebers
+console.log(Object.keys(c1));
+
+// returns all members (instance + prototype)
+for (let key in c1) console.log(key);
+
+console.log(c1.hasOwnProperty('radius'));
+console.log(c1.hasOwnProperty('draw'));
---
+++ 4- Prototypical Inheritance/1- Creating Your Own Prototypical Inheritance/index.js
@@ -0,0 +1,29 @@
+﻿function Shape() {}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+function Circle(radius) {
+    this.radius = radius;
+}
+//Circle.prototype = new Shape();
+Circle.prototype = Object.create(Shape.prototype);
+
+Circle.prototype.draw = function() {
+    console.log('draw Circle');
+};
+
+function Square(size) {
+    this.size = size;
+}
+
+Square.prototype.draw = function() {
+    console.log('draw Square');
+};
+
+const s = new Shape();
+const c = new Circle(1);
+
+console.log(s);
+console.log(c);
