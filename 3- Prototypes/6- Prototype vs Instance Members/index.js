b8f5c6007aa8e6b9919878d082b5afa88b9d6460
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 10:46:02 -0400

    Unit 2 Chapter 6

---
+++ 3- Prototypes/6- Prototype vs Instance Members/index.js
@@ -0,0 +1,47 @@
+﻿/* BEFORE: Instance properties and methods
+
+function Circle(radius) {
+    this.radius = radius;
+
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const c1 = new Circle(1);
+const c2 = new Circle(1);
+
+console.log(c1);
+console.log(c2);
+
+c1.draw();
+*/
+
+/* AFTER: Prototype Members */
+function Circle(radius) {
+    this.radius = radius;
+    this.move = function() {
+        //this.draw();
+        console.log('move');
+    };
+}
+
+Circle.prototype.draw = function() {
+    this.move();
+    console.log('draw');
+};
+
+Circle.prototype.toString = function() {
+    return 'Circle of radius ' + this.radius;
+};
+
+const c1 = new Circle(1);
+const c2 = new Circle(1);
+
+console.log(Circle.prototype === c1.__proto__);
+
+console.log(c1);
+console.log(c2);
+
+c1.draw();
+console.log(c1.toString());
