95d679f129f2c371be9856ca1a516c8a65c3fdb8
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 15:14:15 -0400

    Unit 3 Chapter 2-3

---
+++ 4- Prototypical Inheritance/2- Resetting the Constructor/index.js
@@ -0,0 +1,33 @@
+﻿function Shape() {}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.draw = function() {
+    console.log('draw Circle');
+};
+
+function Square(size) {
+    this.size = size;
+}
+
+Square.prototype.draw = function() {
+    console.log('draw Square');
+};
+
+const s = new Shape();
+const c = new Circle(1);
+
+const c2 = new Circle.prototype.constructor(2);
+
+console.log(s);
+console.log(c);
+console.log(c2);
---
+++ 4- Prototypical Inheritance/3- Resetting the Constructor/index.js
@@ -0,0 +1,19 @@
+﻿function Shape(color) {
+    this.color = color;
+}
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+function Circle(radius, color) {
+    Shape.call(this, color);
+    this.radius = radius;
+}
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+Circle.prototype.draw = function() {
+    console.log('draw Circle');
+};
+
+const c = new Circle(1, 'red');
+console.log(c);
