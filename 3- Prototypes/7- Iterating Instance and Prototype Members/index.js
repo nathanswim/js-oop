4728af7b5f7049f78ff3f533303ecde96d73c017
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 11:40:44 -0400

    Unit 2 Chapter 7

---
+++ 3- Prototypes/7- Iterating Instance and Prototype Members/index.js
@@ -0,0 +1,15 @@
+﻿function Circle(radius) {
+    // instance members
+    this.radius = radius;
+
+    this.move = function() {
+        console.log('move');
+    };
+}
+
+// prototype members
+Circle.prototype.draw = function() {
+    console.log('draw');
+};
+
+const c1 = new Circle(1);
