94f84ce5af2bfacc3d81b0e5e1680e681ac0d30f
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 15:51:55 -0400

    Unit 4 chapter 5

---
+++ 4- Prototypical Inheritance/5- Method Overriding/index.js
@@ -0,0 +1,20 @@
+﻿function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
+function Shape() {}
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+function Circle() {}
+extend(Circle, Shape);
+
+Circle.prototype.duplicate = function() {
+    Shape.prototype.duplicate.call(this);
+    console.log('duplicate Circle');
+};
+
+const c = new Circle(1, 'red');
+console.log(c);
