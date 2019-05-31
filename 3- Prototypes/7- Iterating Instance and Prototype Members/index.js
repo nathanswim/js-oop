1a8f26ada2953395e5e6689e20823934694645f2
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 11:44:34 -0400

    Unit 2 Chapter 7

--- 3- Prototypes/7- Iterating Instance and Prototype Members/index.js
+++ 3- Prototypes/7- Iterating Instance and Prototype Members/index.js
@@ -13,3 +13,12 @@ Circle.prototype.draw = function() {
 };
 
 const c1 = new Circle(1);
+
+// only returns instance memebers
+console.log(Object.keys(c1));
+
+// returns all members (instance + prototype)
+for (let key in c1) console.log(key);
+
+console.log(c1.hasOwnProperty('radius'));
+console.log(c1.hasOwnProperty('draw'));
