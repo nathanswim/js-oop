bec99998ca9b142faa2c4753a65183fed7b2c0f6
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 09:36:25 -0400

    Unit 2 Chapter 5

---
+++ 3- Prototypes/5- Constructor Prototypes/index.js
@@ -0,0 +1,21 @@
+﻿// Object.getPrototypeOf(myObj);
+// myObj.__proto__ (parent of myObj)
+
+// constructors also have a prototype property
+function Circle(radius) {
+    this.radius = radius;
+}
+
+const circle = new Circle(1);
+
+let obj = {};
+console.log(obj.__proto__);
+console.log(Object.prototype); // these are both the same
+
+let array = [];
+
+console.log(array.__proto__);
+console.log(Array.prototype);
+
+console.log(circle.__proto__);
+console.log(Circle.prototype);
