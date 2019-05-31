8f1731353302d21fce216dd30c64e52edb7452a5
Nathan Swim <nathan.swim@med2020.ca>
2019-05-31 16:12:37 -0400

    Unit 4 Chapter 8

---
+++ 4- Prototypical Inheritance/8- Mixins/index.js
@@ -0,0 +1,37 @@
+﻿function mixin(target, ...sources) {
+    Object.assign(target, ...sources);
+}
+
+const canEat = {
+    eat: function() {
+        this.hunger--;
+        console.log('eating');
+    }
+};
+const canWalk = {
+    walk: function() {
+        console.log('walking');
+    }
+};
+
+const canSwim = {
+    swim: function() {
+        console.log('swimming');
+    }
+};
+
+function Person() {}
+mixin(Person.prototype, canEat, canWalk);
+
+function Goldfish() {}
+mixin(Goldfish.prototype, canEat, canSwim);
+
+function Duck() {}
+mixin(Duck.prototype, canWalk, canEat, canSwim);
+
+const person = new Person();
+console.log(person);
+const fish = new Goldfish();
+console.log(fish);
+const duck = new Duck();
+console.log(duck);
