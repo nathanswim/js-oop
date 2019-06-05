a50add8cd702b03e27dfd299fc02cff205e98025
Nathan Swim <nathan.swim@gmail.com>
2019-06-05 19:40:54 -0400

    Unit 5 chapter 3

---
+++ 5- ES6 Classes/2- Static Methods/index.js
@@ -0,0 +1,24 @@
+﻿
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    // Instance method
+    draw() {
+        console.log('draw');
+    }
+
+    // Static method 
+    static parse(str){
+        const radius = JSON.parse(str).radius;
+        return new Circle(radius);
+
+    }
+}
+
+const circle =  Circle.parse('{ "radius": 13}');
+
+console.log(circle);
+
+
