462da2c70a72caf467b5454e8abca0430ee85671
Nathan Swim <nathan.swim@gmail.com>
2019-06-08 17:10:05 -0400

    unit 5 chapter 3

--- 5- ES6 Classes/3- The This Keyword/index.js
+++ 5- ES6 Classes/3- The This Keyword/index.js
@@ -1,11 +1,26 @@
-﻿const Circle = function() {
+﻿'use strict';
+
+const Circle = function() {
   this.draw = function() {
     console.log(this);
   };
 };
 
 const c = new Circle();
+// Method call: this points to the current object
 c.draw();
 
 const draw = c.draw;
 console.log(draw);
+// function call: by default this will point to Window/Global
+draw();
+
+class Circle2 {
+  draw(){
+    console.log(this);
+  }
+}
+
+const c2 = new Circle2();
+const draw2 = c.draw;
+draw2();
